e0f95eb9a2b63c373ed669120d5de55df3a73bfa
Stud. Marco Perno <m.perno.0735@vallauri.net>
2020-10-15 13:20:14 +0200

    inizio FTP

---
+++ Sistemi/Socket/Socket_02/Socket02_Cl/Socket02_Cl/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Socket02_Cl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtIPServer = new System.Windows.Forms.TextBox();
+            this.txtPortaServer = new System.Windows.Forms.TextBox();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.btnApri = new System.Windows.Forms.Button();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.btnInvia = new System.Windows.Forms.Button();
+            this.lstLog = new System.Windows.Forms.ListBox();
+            this.btnPulisci = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.groupBox1.Controls.Add(this.btnInvia);
+            this.groupBox1.Controls.Add(this.btnTest);
+            this.groupBox1.Controls.Add(this.btnApri);
+            this.groupBox1.Controls.Add(this.txtFile);
+            this.groupBox1.Controls.Add(this.txtPortaServer);
+            this.groupBox1.Controls.Add(this.txtIPServer);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(612, 156);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Dati connesione server";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.groupBox2.Controls.Add(this.btnPulisci);
+            this.groupBox2.Controls.Add(this.lstLog);
+            this.groupBox2.Location = new System.Drawing.Point(12, 174);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(612, 265);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Log operazione FTP Client";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP Server";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Porta server";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(56, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "File da inviare";
+            // 
+            // txtIPServer
+            // 
+            this.txtIPServer.Location = new System.Drawing.Point(141, 36);
+            this.txtIPServer.Name = "txtIPServer";
+            this.txtIPServer.Size = new System.Drawing.Size(158, 20);
+            this.txtIPServer.TabIndex = 3;
+            // 
+            // txtPortaServer
+            // 
+            this.txtPortaServer.Location = new System.Drawing.Point(141, 66);
+            this.txtPortaServer.Name = "txtPortaServer";
+            this.txtPortaServer.Size = new System.Drawing.Size(100, 20);
+            this.txtPortaServer.TabIndex = 4;
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(141, 95);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(100, 20);
+            this.txtFile.TabIndex = 5;
+            // 
+            // btnApri
+            // 
+            this.btnApri.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnApri.Location = new System.Drawing.Point(264, 93);
+            this.btnApri.Name = "btnApri";
+            this.btnApri.Size = new System.Drawing.Size(35, 23);
+            this.btnApri.TabIndex = 6;
+            this.btnApri.Text = "...";
+            this.btnApri.UseVisualStyleBackColor = true;
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(323, 34);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(77, 48);
+            this.btnTest.TabIndex = 7;
+            this.btnTest.Text = "Test connessione";
+            this.btnTest.UseVisualStyleBackColor = true;
+            // 
+            // btnInvia
+            // 
+            this.btnInvia.Enabled = false;
+            this.btnInvia.Location = new System.Drawing.Point(323, 93);
+            this.btnInvia.Name = "btnInvia";
+            this.btnInvia.Size = new System.Drawing.Size(75, 23);
+            this.btnInvia.TabIndex = 8;
+            this.btnInvia.Text = "Invia";
+            this.btnInvia.UseVisualStyleBackColor = true;
+            // 
+            // lstLog
+            // 
+            this.lstLog.FormattingEnabled = true;
+            this.lstLog.Location = new System.Drawing.Point(6, 58);
+            this.lstLog.Name = "lstLog";
+            this.lstLog.Size = new System.Drawing.Size(597, 186);
+            this.lstLog.TabIndex = 0;
+            // 
+            // btnPulisci
+            // 
+            this.btnPulisci.Location = new System.Drawing.Point(499, 19);
+            this.btnPulisci.Name = "btnPulisci";
+            this.btnPulisci.Size = new System.Drawing.Size(75, 23);
+            this.btnPulisci.TabIndex = 1;
+            this.btnPulisci.Text = "Pulisci";
+            this.btnPulisci.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(636, 451);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FTP - Client";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnInvia;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Button btnApri;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.TextBox txtPortaServer;
+        private System.Windows.Forms.TextBox txtIPServer;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox lstLog;
+        private System.Windows.Forms.Button btnPulisci;
+    }
+}
+

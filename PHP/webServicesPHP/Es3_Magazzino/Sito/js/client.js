deb5547c733373693c47ea313c566928de3dc8da
marco.perno2002 <marco.perno2002@outlook.it>
2020-10-20 16:04:56 +0200

    Es3_MagazzinoCompleto

--- PHP/webServicesPHP/Es3_Magazzino/Sito/js/client.js
+++ PHP/webServicesPHP/Es3_Magazzino/Sito/js/client.js
@@ -1,6 +1,13 @@
 
 function init(){
-    alert("D");
+    caricaTabella2();
+    $("#btnInserisci").click(inserisci);
+    //$("#btnLogin").click(login);
+}
+
+function caricaTabella()
+{
+    $("#tabBody").empty();
     let modelloDati={};
     modelloDati.servizio="getMagazzino";
     $.ajax({
@@ -11,18 +18,85 @@ function init(){
             var ris=JSON.parse(json);
             if(ris["code"]=="1")
             {
-                alert("log ok");
+                //alert("Cericamento avvenuto con successo");
+                $.each(ris["magazzino"], function (indexInArray, valueOfElement) {
+                    let tr=$("<tr></tr>");
+                    for(let i=0;i<5;i++)
+                    {
+                        if(i==0)
+                            aggiungiTd(tr, ris["magazzino"][indexInArray].nome);
+                        else if(i==1)
+                            aggiungiTd(tr, ris["magazzino"][indexInArray].quantita)
+                        else if(i==2)
+                            aggiungiTd(tr, ris["magazzino"][indexInArray].descrizione);
+                        else if(i==3)
+                        {
+                            let button=$("<button >compra</button>");
+                            button.click({"i":ris["magazzino"][indexInArray].codArticolo},compra);
+                            let td = $("<td ></td>").append(button);
+                            tr.append(td);
+                        }
+                        else
+                        {
+                            let button=$("<button >Elimina</button>");
+                            button.click({"i":ris["magazzino"][indexInArray].codArticolo},elimina);
+                            let td = $("<td ></td>").append(button);
+                            tr.append(td);
+                        }
+                            
+                            
+                    }
+                    $("#tabBody").append(tr);
+                    
+                });
             }
             else
             {
-                alert("log no");
+                alert("Cericamento non avvenuto con successo");
             }
         },
         error: function(e)
         {
-            alert("log no");
+            alert("err");
         }
     });
+}
+
+function inserisci()
+{
+    let modelloDati={};
+    modelloDati.servizio="postInserisci";
+    modelloDati.nome=$("#txtNome").val();
+    modelloDati.quantita=$("#txtQuantita").val();
+    modelloDati.descrizione=$("#txtDescr").val();
+    $.ajax({
+        url: "server.php",
+        type: 'POST',
+        data: JSON.stringify(modelloDati),
+        success: function (json) {
+            var ris=JSON.parse(json);
+            if(ris["code"]=="1")
+            {
+                alert("Cericamento avvenuto con successo");
+                caricaTabella2();
+            }
+            else if(ris["code"]=="0")
+            {
+                alert("Cericamento non avvenuto con successo");
+            }
+        },
+        error: function(e)
+        {
+            alert("err");
+        }
+    });
+}
+
+function aggiungiTd(tr, str)
+{
+    let td=$("<td></td>");
+    td.text(str);
+    tr.append(td);
 }
 
 
@@ -33,3 +107,142 @@ function errore(e){
 function stampa(msg){
     alert(msg);
 }
+function compra(event) {  
+    let modelloDati={};
+    modelloDati.servizio="postCompra";
+    modelloDati.codArticolo=event.data.i;
+    $.ajax({
+        url: "server.php",
+        type: 'POST',
+        data: JSON.stringify(modelloDati),
+        success: function (json) {
+            var ris=JSON.parse(json);
+            if(ris["code"]=="1")
+            {
+                alert("Cericamento avvenuto con successo");
+            }
+            else if(ris["code"]=="0")
+            {
+                alert("eliminato");
+            }
+            
+            caricaTabella2();
+        },
+        error: function(e)
+        {
+            alert("err");
+        }
+    });
+}
+function elimina(event) {  
+    let modelloDati={};
+    modelloDati.servizio="postElimina";
+    modelloDati.codArticolo=event.data.i;
+    $.ajax({
+        url: "server.php",
+        type: 'POST',
+        data: JSON.stringify(modelloDati),
+        success: function (json) {
+            var ris=JSON.parse(json);
+            
+            alert("Eliminazione avvenuto con successo");
+            caricaTabella2();
+            
+        },
+        error: function(e)
+        {
+            alert("err ");
+        }
+    });
+}
+
+function login(event) {
+    let modelloDati={};
+    modelloDati.servizio="postLogin";
+    modelloDati.nome = "Marco";
+    modelloDati.pwd = "pwd";
+
+    $.ajax({
+        url: "server.php",
+        type: 'POST',
+        data: JSON.stringify(modelloDati),
+        success: function (json) {
+            var ris=JSON.parse(json);
+            if(ris['code']=="1")
+            {
+                alert("Login avvenuto con successo");
+            }
+            else
+            {
+                alert("No Login avvenuto con successo");
+            }
+            //document.location ="Sito/prova.html";
+            caricaTabella2();
+            
+        },
+        error: function(e)
+        {
+            alert("err ");
+        }
+    });
+}
+
+
+
+function caricaTabella2(){
+    let modelloDati={};
+    modelloDati.servizio="getMagazzino";
+
+    $.ajax({
+        url: "server.php",
+        type: 'Post',
+        data: JSON.stringify(modelloDati),
+        success: function (json) {
+            var ris=JSON.parse(json);
+            $("#tabScroll").empty();
+            if(ris['code']=="1")
+            {
+                $.each(ris['magazzino'], function (indexInArray, valueOfElement) { 
+                    let tr=$("<tr></tr>")
+                    aggiungiTd(tr, ris['magazzino'][indexInArray]['nome']);
+                    aggiungiTd(tr, ris['magazzino'][indexInArray]['quantita']);
+                    aggiungiTd(tr, ris['magazzino'][indexInArray]['descrizione']);
+                    aggiungiBottone(tr, ris['magazzino'][indexInArray]["codArticolo"], "compra");
+                    aggiungiBottone(tr, ris['magazzino'][indexInArray]["codArticolo"], "elimina");
+                    $("#tabScroll").append(tr);
+                });
+            }
+            else
+            {
+                
+            }
+            //document.location ="Sito/prova.html";
+            
+        },
+        error: function(e)
+        {
+            alert("err ");
+        }
+    });
+}
+
+function aggiungiBottone(tr, i, text)
+{
+    let td=$("<td></td>");
+    
+    let button = $("<button>"+text+"</button>");
+    if(text=="compra")
+    {
+        button.click({"i": i},  compra);
+
+    }
+    else
+    {
+        button.click({"i": i},  elimina);
+
+    }
+
+    td.append(button);
+    tr.append(td);
+}
+
--- PHP/webServicesPHP/Es3_Magazzino/server.php
+++ PHP/webServicesPHP/Es3_Magazzino/server.php
@@ -1,5 +1,72 @@
 <?php
-    //Prende tutti i dati (oggetti forniti in data nella chiamata $ajax) della richiesta
+
+    function elimina($pdo, $codArticolo)
+    {
+        $sql="DELETE  FROM articolo WHERE codArticolo = :codArticolo";
+
+        $sth= $pdo->prepare($sql);
+
+        $sth->bindParam(':codArticolo', $codArticolo, PDO::PARAM_INT);
+
+        $sth->execute();
+    }
+
+    function controlloCategoria($pdo, $descr)
+    {
+        $sql="SELECT codCategoria FROM settore WHERE descrizione = :descr";
+
+        $sth= $pdo->prepare($sql);
+
+        $sth->bindParam(':descr', $descr, PDO::PARAM_STR);
+
+        $sth->execute();
+
+        $data=$sth->fetch();
+        
+        $count= $sth->rowCount();
+        if($count==0)
+        {
+            $sql="INSERT INTO settore (  descrizione) VALUES (  :descr)";
+
+            $sth= $pdo->prepare($sql);
+
+            $sth->bindParam(':descr', $descr, PDO::PARAM_STR);
+
+            $sth->execute();
+
+            $sql="SELECT codCategoria FROM settore WHERE descrizione = :descr";
+
+            $sth= $pdo->prepare($sql);
+
+            $sth->bindParam(':descr', $descr, PDO::PARAM_STR);
+
+            $sth->execute();
+
+            $data=$sth->fetch();
+        }
+        
+        return $data["codCategoria"];
+    }
+
+    function inserisciArticolo($pdo, $modelloDati)
+    {
+        $sql="INSERT INTO articolo ( nome, quantita, codCategoria) VALUES ( :nome,  :quantita, :codCategoria)";
+
+        $sth= $pdo->prepare($sql);
+
+        $nome=$modelloDati["nome"];
+        $codCategoria=$modelloDati["codCategoria"];
+        $quantita=$modelloDati["quantita"];
+
+        $sth->bindParam(':nome', $nome, PDO::PARAM_STR);
+        $sth->bindParam(':quantita', $quantita, PDO::PARAM_INT);
+        $sth->bindParam(':codCategoria', $codCategoria, PDO::PARAM_INT);
+
+        $sth->execute();
+    }
+
+
+
     $json = file_get_contents('php://input');
     //Traduce l'oggetto json in oggetto php
     $modelloDati = json_decode($json,true);
@@ -18,7 +85,7 @@
 
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-            $sql="SELECT art.nome, art.quantita, sett.descrizione FROM articolo as art, settore as sett WHERE art.codCategoria = sett.codCategoria ORDER BY art.nome ASC";
+            $sql="SELECT art.codArticolo, art.nome, art.quantita, sett.descrizione FROM articolo as art, settore as sett WHERE art.codCategoria = sett.codCategoria ORDER BY art.nome ASC";
 
             $sth= $pdo->prepare($sql);
 
@@ -31,17 +98,134 @@
                 $vet[$cont]->nome = $rows['nome'];
                 $vet[$cont]->quantita = $rows['quantita'];
                 $vet[$cont]->descrizione = $rows['descrizione'];
+                $vet[$cont]->codArticolo = $rows['codArticolo'];
                 $cont++;
             }
             $modelloDati['code']=1;
             $modelloDati['magazzino']=$vet;
             echo json_encode($modelloDati);
+        break;
 
-            
+        //////////////////////
 
+        case "postInserisci":
+            $pdo=new PDO('mysql:host=localhost;dbname=es3magazzino;charset=utf8','root','');
+
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $sql="SELECT * FROM articolo WHERE nome = :nome";
+
+            $sth= $pdo->prepare($sql);
+
+            $utente=$modelloDati["nome"];
+
+            $sth->bindParam(':nome', $utente, PDO::PARAM_STR);
+
+            $sth->execute();
+
+            $data=$sth->fetch();
             
+            $count= $sth->rowCount();
+
+            if($count==0)
+            {
+                $modelloDati["codCategoria"] = controlloCategoria($pdo, $modelloDati["descrizione"]);
+                inserisciArticolo($pdo, $modelloDati);
+                
+                $modelloDati["code"]=1;
+            }
+            else
+            {
+                $modelloDati["code"]=0;
+            }
+            
+            echo json_encode($modelloDati);
         break;
         
+        ///////
+        case "postCompra":
+            $pdo=new PDO('mysql:host=localhost;dbname=es3magazzino;charset=utf8','root','');
+
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $sql="SELECT quantita FROM articolo WHERE codArticolo = :codArticolo";
+
+            $sth= $pdo->prepare($sql);
+
+            $codArticolo=$modelloDati["codArticolo"];
+
+            $sth->bindParam(':codArticolo', $codArticolo, PDO::PARAM_INT);
+
+            $sth->execute();
+
+            $data=$sth->fetch();
+
+            if($data["quantita"]!="1")
+            {
+                ///rimuovi 1
+                $sql="UPDATE articolo SET quantita = quantita -1 WHERE articolo.codArticolo = :codArticolo";
+
+                $sth= $pdo->prepare($sql);
+    
+                $codArticolo=$modelloDati["codArticolo"];
+    
+                $sth->bindParam(':codArticolo', $codArticolo, PDO::PARAM_INT);
+    
+                $sth->execute();
+
+                $modelloDati["code"]=1;
+            }
+            else
+            {
+                elimina($pdo, $modelloDati["codArticolo"]);
+                $modelloDati["code"]=0;
+            }
+
+            echo json_encode($modelloDati);
+            
+        break;
+
+
+        case "postElimina":
+            $pdo=new PDO('mysql:host=localhost;dbname=es3magazzino;charset=utf8','root','');
+
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            elimina($pdo, $modelloDati["codArticolo"]);
+
+            $modelloDati["code"]=1;
+
+            echo json_encode($modelloDati);
+        break;
+
+        case "postLogin":
+            $pdo=new PDO('mysql:host=localhost;dbname=es2login;charset=utf8','root','');
+
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $sql="SELECT * FROM utenti WHERE Pwd = md5(:pwd)";
+
+            $sth= $pdo->prepare($sql);
+
+            $pwd=$modelloDati["pwd"];
+
+            $sth->bindParam(':pwd', $pwd, PDO::PARAM_STR);
+
+            $sth->execute();
+
+            $count= $sth->rowCount();
+
+            if($count>0)
+            {
+                $modelloDati["code"]=1;
+            }
+            else
+            {
+                $modelloDati["code"]=0;
+            }
+
+            echo json_encode($modelloDati);
+        break;
         
         //Caso in cui non entra in nessun case 
         //  -> richiedo un servizio che non esiste in questo server
